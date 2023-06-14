78f1f060f5eac83be2b92f3c43a1196c1ef20150
Stanislav Povedenok <stas.egor.povedenok@gmail.com>
2023-06-14 23:17:12 +0300

    added navigation component

---
+++ app/src/main/java/com/example/todoapp/ui/fragments/AllTasksFragment.kt
@@ -0,0 +1,38 @@
+package com.example.todoapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentAllTasksBinding
+
+
+class AllTasksFragment : Fragment() {
+
+    private var _binding: FragmentAllTasksBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAllTasksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.root.setOnClickListener {
+            findNavController().navigate(R.id.action_allTasksFragment_to_taskFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
