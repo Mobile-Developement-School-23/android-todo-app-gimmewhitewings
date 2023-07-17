f137d15808041bdf33c6dc17deb993307d1137e0
Stanislav Povedenok <stas.egor.povedenok@gmail.com>
2023-07-17 12:30:46 +0300

    Added close button to SettingsFragment

--- app/src/main/java/com/example/todoapp/ui/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/fragments/settings/SettingsFragment.kt
@@ -11,11 +11,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -29,6 +36,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
 import com.example.todoapp.appComponent
 import com.example.todoapp.ui.fragments.settings.viewmodel.SettingsViewModel
@@ -63,6 +71,7 @@ class SettingsFragment : Fragment() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     private fun SettingsScreen(viewModel: SettingsViewModel) {
         val viewState = viewModel.uiState.collectAsState()
@@ -70,32 +79,53 @@ class SettingsFragment : Fragment() {
             viewModel.setAppTheme(it)
             activity?.recreate()
         }
+        val onCloseButtonClick: () -> Unit = {
+            findNavController().popBackStack()
+        }
         Surface(
-            modifier = Modifier.background(
-                color = TodoAppTheme.colors.backPrimary
-            )
+            modifier = Modifier.background(color = TodoAppTheme.colors.backPrimary)
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        color = TodoAppTheme.colors.backPrimary
-                    ),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                listOf(
-                    ApplicationTheme.DAY,
-                    ApplicationTheme.SYSTEM,
-                    ApplicationTheme.NIGHT
-                ).forEach { applicationTheme ->
-                    ApplicationThemeButton(
-                        isSelected = applicationTheme == viewState.value.chosenMode,
-                        onButtonClick = onButtonClick,
-                        applicationTheme = applicationTheme
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = TodoAppTheme.colors.backPrimary
+                        ),
+                        navigationIcon = {
+                            IconButton(onClick = onCloseButtonClick) {
+                                Icon(
+                                    imageVector = Icons.Filled.Close,
+                                    contentDescription = "close",
+                                    tint = TodoAppTheme.colors.labelPrimary
+                                )
+                            }
+                        },
+                        title = {}
                     )
+                },
+            ) { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .padding(top = innerPadding.calculateTopPadding())
+                        .fillMaxSize()
+                        .background(color = TodoAppTheme.colors.backPrimary),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    listOf(
+                        ApplicationTheme.DAY,
+                        ApplicationTheme.SYSTEM,
+                        ApplicationTheme.NIGHT
+                    ).forEach { applicationTheme ->
+                        ApplicationThemeButton(
+                            isSelected = applicationTheme == viewState.value.chosenMode,
+                            onButtonClick = onButtonClick,
+                            applicationTheme = applicationTheme
+                        )
+                    }
                 }
             }
+
         }
     }
 
